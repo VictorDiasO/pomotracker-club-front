4f38d7b83b40fffbe86de9e73b03924bb5e3951b
VictorDiasO <victordiasdeveloper@gmail.com>
2023-08-05 02:39:54 -0300

    Adding themes to application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
+"use client";
+import { useTheme } from "next-themes";
+import { Roboto } from "next/font/google";
+import { useEffect, useState } from "react";
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: '100'
+});
+
 export default function Home() {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  const [ mounted, setMounted ] = useState<boolean>();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
-    <main>
+    <main className="flex content-center items-center">
+      <div className="flex content-center items-center shrink-0">
+        <p className="dark:text-red-900 text-black">The current theme is: ${currentTheme}</p>
+        <button onClick={() => setTheme('light')}>Light mode</button>
+        <button onClick={() => setTheme('dark')}>Dark mode</button>
+      </div>
     </main>
-  )
+  );
 }
---
+++ src/app/providers.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { ThemeProvider } from "next-themes";
+import React, { useEffect, useState } from "react";
+
+export default function Providers({ children }: { children: React.ReactNode}) {
+  const [ mounted, setMounted ] = useState<boolean>();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return <>{children}</>;
+
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+}
