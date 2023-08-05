30dfa2ecf46c05fd5462d8d82f3576cb1234a8cd
VictorDiasO <victordiasdeveloper@gmail.com>
2023-08-05 15:05:42 -0300

    Setting up the basic UI on the main page yet

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { ThemeProvider } from "next-themes";
 import React, { useEffect, useState } from "react";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function Providers({ children }: { children: React.ReactNode}) {
   const [ mounted, setMounted ] = useState<boolean>();
