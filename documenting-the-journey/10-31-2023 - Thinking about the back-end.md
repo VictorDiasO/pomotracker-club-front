49a0e0ebf8d90d6eb19b964ce07231f9d38173f2
VictorDiasO <victordiasdeveloper@gmail.com>
2023-10-31 22:09:15 -0300

    Adding a new note to my journey | Adding a Database to the application and setting it up

---
+++ documenting-the-journey/10-31-2023 - Thinking about the back-end.md
@@ -0,0 +1,14 @@
+I started to look and search ways to design the back-end architecture for this application... but as I writed on README, this project have the main purpose of work as a company.
+
+What "Work as a company" means?
+Easy, work as a company is basically focus on "profit"/scale. Focus in achieve the most number of users and features without having to worry too much about costs... so I need to start from a MVP (Minimal Viable Product), and in this case my budget for this MVP is $0 or less hahahaha.
+
+I forgot the idea of creating a complex back-end for this application, and maybe we can use the "/api" route from NextJS instead for the MVP. With this simple approach we can validate the idea, put people to use it and etc.
+
+But what I've "decided" about the back-end is:
+- It will be made using NestJS;
+- I will use a Relational database
+- I will TRY to use open source solutions to host my database (it depends on the prices hahaha).
+
+The architecture of the back-end application will be very simple, maybe I wont need to use a Load Balancer or something similar because I have other ideas to make the requests cheap for our pocket hahahaha.
+
--- src/components/TasksModal.tsx
+++ src/components/TasksModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { dynamicButtonColors } from "@/helpers";
-import { GearSix, PencilSimple, X } from "@phosphor-icons/react";
-import { Button, Collapse, CollapseProps, Form, Input, InputNumber, Modal } from "antd";
+import { PencilSimple, X } from "@phosphor-icons/react";
+import { Button, Collapse, Form, Input, InputNumber, Modal } from "antd";
 import { useTheme } from "next-themes";
 import { Dispatch, SetStateAction, useState } from "react";
 
@@ -10,30 +10,34 @@ interface ITasksModal {
   setOpenSettingsModal: (value: SetStateAction<boolean>) => void;
 }
 
+interface ITask {
+  id: number;
+}
+
 const tasksApiModel = [
   {
-    id: "2",
+    id: 2,
     title: "First task",
     description: "A very huuuge description that I dont even know how I will show it to the user, but okay, it doesn't matter now. But Im really worried about it",
     estimated_pomodoros: 15,
     done_pomodoros: 2
   },
   {
-    id: "3",
+    id: 3,
     title: "Second task",
     description: "A very huuuge description that I dont even know how I will show it to the user, but okay, it doesn't matter now. But Im really worried about it",
     estimated_pomodoros: 15,
     done_pomodoros: 2
   },
   {
-    id: "4",
+    id: 4,
     title: "Third task",
     description: "A very huuuge description that I dont even know how I will show it to the user, but okay, it doesn't matter now. But Im really worried about it",
     estimated_pomodoros: 15,
     done_pomodoros: 2
   },
   {
-    id: "5",
+    id: 5,
     title: "Forth task",
     description: "A very huuuge description that I dont even know how I will show it to the user, but okay, it doesn't matter now. But Im really worried about it",
     estimated_pomodoros: 15,
@@ -46,39 +50,24 @@ export const TasksModal = ({
   setOpenSettingsModal
 }: ITasksModal) => {
   const [openCreationTaskModal, setOpenCreationTaskModal] = useState<boolean>(false);
+  const [editTaskModalId, setEditTaskModalId] = useState<number | null>(null);
 
   const [ form ] = Form.useForm();
 
   const {
-    theme,
-    setTheme
+    theme
   } = useTheme();
 
   const handleSave = () => {}
 
-  const items: CollapseProps['items'] = [
-    {
-      key: '1',
-      label: 'This is panel header 2',
-      children: <p>Making it show</p>,
-    }
-  ];
-
-  const genExtra = () => (
-    <div className="flex flex-row justify-between gap-4">
-      <GearSix size={24}
-        onClick={(event) => {
-          // If you don't want click extra trigger collapse, you can prevent this:
-          // event.stopPropagation();
-          console.log('Settings');
-        }}
-      />
-      <PencilSimple size={24}
-        onClick={() => {
-          console.log('Edit');
-        }}
-      />
-    </div>
+  const genExtra = (taskId: number) => (
+    <PencilSimple size={24}
+      onClick={(event) => {
+        event.stopPropagation();
+        console.log('Edit', taskId);
+        setEditTaskModalId(taskId);
+      }}
+    />
   );
 
 
@@ -108,10 +97,10 @@ export const TasksModal = ({
           <div className="flex flex-row justify-between">
             <div></div>
             <button
-              className={`font-bold text-lg p-2 w-full mb-3 rounded-2xl ${dynamicButtonColors.secondaryDynamicButtonColors(theme)}`}
+              className={`font-bold text-lg p-2 w-full mb-3 rounded-2xl ${dynamicButtonColors.primaryDynamicButtonColors(theme)}`}
               onClick={() => setOpenCreationTaskModal(!openCreationTaskModal)}
             >
-              + Add Task
+              {openCreationTaskModal ? 'See Tasks' : '+ Add Task'}
             </button>
             <div></div>
           </div>
@@ -121,13 +110,14 @@ export const TasksModal = ({
               setOpen={setOpenCreationTaskModal}
             />
           )}
+          {editTaskModalId}
           {!openCreationTaskModal && (
             <>
               <Collapse items={tasksApiModel.map((task) => {
                 return {
                   key: task.id,
                   label: task.title,
-                  extra: genExtra(),
+                  extra: genExtra(task.id),
                   children: <div className="flex flex-col gap-2">
                     <div>
                       <h3
@@ -156,15 +146,20 @@ export const TasksModal = ({
   );
 }
 
-const CreateNewTask = ({open, setOpen}: any) => {
-  const [ form ] = Form.useForm();
+const formItems = {
+  taskTitle: 'taskTitle',
+  taskDesc: 'taskDescription',
+  estimatedPomos: 'estimatedPomodoros',
+  donePomos: 'donePomodoros'
+}
 
-  const formItems = {
-    taskTitle: 'taskTitle',
-    taskDesc: 'taskDescription',
-    estimatedPomos: 'estimatedPomodoros',
-    donePomos: 'donePomodoros'
-  }
+type TCreateNewTask = {
+  open: boolean,
+  setOpen: Dispatch<SetStateAction<boolean>>
+}
+
+const CreateNewTask = ({open, setOpen}: TCreateNewTask) => {
+  const [ form ] = Form.useForm();
 
   const handleSave = async () => {}
 
@@ -173,10 +168,7 @@ const CreateNewTask = ({open, setOpen}: any) => {
       className="mt-6"
     >
       <Form
-        layout="horizontal"
-        labelCol={{ span: 12 }}
-        wrapperCol={{ span: 14 }}
-        labelAlign="left"
+        layout="vertical"
         style={{ maxWidth: 600 }}
         form={form}
         onFinish={handleSave}
@@ -194,7 +186,80 @@ const CreateNewTask = ({open, setOpen}: any) => {
           label="Description"
           name={formItems.taskDesc}
         >
+          <Input.TextArea
+            placeholder="Insert a good description"
+            className="w-full"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Estimated Pomodoros"
+          name={formItems.estimatedPomos}
+        >
+          <InputNumber
+            placeholder="ex: 7"
+            className="w-full"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Done Pomodoros"
+          name={formItems.donePomos}
+        >
+          <InputNumber
+            placeholder="ex: 2"
+            className="w-full"
+          />
+        </Form.Item>
+        <div className="flex justify-between mt-5 max-w-[100%]">
+          <div></div>
+          <Button
+            htmlType="submit"
+            type="primary"
+            className="flex bg-blue-400"
+          >
+            Create task
+          </Button>
+          <div></div>
+        </div>
+      </Form>
+    </div>
+  );
+}
+
+type TEditTask = {
+  open: boolean,
+  setOpen: Dispatch<SetStateAction<boolean>>,
+  taskContent: ITask
+}
+
+const EditTask = ({open, setOpen, taskContent}: any) => {
+  const [ form ] = Form.useForm();
+
+  const handleSave = async () => {}
+
+  return (
+    <div
+      className="mt-6"
+    >
+      <Form
+        layout="vertical"
+        style={{ maxWidth: 600 }}
+        form={form}
+        onFinish={handleSave}
+      >
+        <Form.Item
+          label="Task Title"
+          name={formItems.taskTitle}
+        >
           <Input
+            placeholder="Boring title for this task"
+            className="w-full"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Description"
+          name={formItems.taskDesc}
+        >
+          <Input.TextArea
             placeholder="Insert a good description"
             className="w-full"
           />
